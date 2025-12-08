a67618937f7398965cc2e5d778cd43fe0a2f3af4
Bridgetamana <amanabridget03@gmail.com>
2025-12-08 19:17:40 +0100

    feat: integrate prism-react-renderer for syntax highlighting in CodeBlock component

--- components/docs/code-block.tsx
+++ components/docs/code-block.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react"
 import { CheckIcon, CopyIcon } from "@phosphor-icons/react"
+import { Highlight, themes } from "prism-react-renderer"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 
@@ -21,7 +22,7 @@ export function CodeBlock({ code, language = "tsx", className, ...props }: CodeB
 
     return (
         <div className={cn("relative group rounded-base border-2 border-border bg-neutral-950 text-neutral-50 font-mono text-sm", className)} {...props}>
-            <div className="absolute right-4 top-4 opacity-0 group-hover:opacity-100 transition-opacity">
+            <div className="absolute right-4 top-4 opacity-0 group-hover:opacity-100 transition-opacity z-10">
                 <Button
                     size="icon"
                     variant="neutral"
@@ -37,11 +38,23 @@ export function CodeBlock({ code, language = "tsx", className, ...props }: CodeB
                 </Button>
             </div>
             <div className="overflow-x-auto p-4">
-                <pre>
-                    <code className={`language-${language}`}>
-                        {code}
-                    </code>
-                </pre>
+                <Highlight
+                    theme={themes.dracula}
+                    code={code}
+                    language={language}
+                >
+                    {({ style, tokens, getLineProps, getTokenProps }) => (
+                        <pre style={{ ...style, background: "transparent" }}>
+                            {tokens.map((line, i) => (
+                                <div key={i} {...getLineProps({ line })}>
+                                    {line.map((token, key) => (
+                                        <span key={key} {...getTokenProps({ token })} />
+                                    ))}
+                                </div>
+                            ))}
+                        </pre>
+                    )}
+                </Highlight>
             </div>
         </div>
     )
